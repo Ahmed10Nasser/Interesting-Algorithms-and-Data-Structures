61e186ecb4c52a575921f644e16571a1e8713fab
Ahmed Nasser Zaki <anm1611999@gmail.com>
2021-12-12 23:37:30 +0200

    Update Merge Sort.cpp

--- Six Sorting Algorithms/Merge Sort.cpp
+++ Six Sorting Algorithms/Merge Sort.cpp
@@ -10,60 +10,36 @@
 */
 
 #include<bits/stdc++.h>
-#define FIO ios_base::sync_with_stdio(0);cin.tie(0);
 using namespace std;
-typedef long long ll;
-
-int const N=2e5+5, M=127+5, MOD=1e9+7, OO=0x3f3f3f3f;
-
-int n;
-int arr[N];
-
-
-void Merge(int l,int md, int r)
-{
-    vector <int> L;
-    vector <int> R;
-    for(int i=l; i<=md; i++)
-        L.push_back(arr[i]);
-    for(int i=md+1; i<=r; i++)
-        R.push_back(arr[i]);
-    L.push_back(OO);
-    R.push_back(OO);
-    int i=0,j=0;
-    for(int k=l; k<=r; k++)
-    {
-        if(L[i]<=R[j])
-            arr[k]=L[i++];
-        else
-            arr[k]=R[j++];
-    }
+ 
+void merge(int l, int md, int r, vector<int>& arr){
+  vector<int>L,R;
+  for(int i=l; i<=md; i++) L.push_back(arr[i]);
+  for(int i=md+1; i<=r; i++) R.push_back(arr[i]);
+  L.push_back(INT_MAX);
+  R.push_back(INT_MAX);
+  int lp=0, rp=0;
+  for(int i=l; i<=r; i++){
+    if(L[lp]<=R[rp]) arr[i]=L[lp++];
+    else arr[i]=R[rp++];
+  }
 }
-
-
-void mergeSort(int l, int r)
-{
-    if(l>=r)
-        return;
-    int md=(l+r)/2;
-    mergeSort(l,md);
-    mergeSort(md+1,r);
-    Merge(l,md,r);
+ 
+void mergeSort(int l, int r, vector<int>& arr){
+  if(l>=r) return;
+  int md=(l+r)/2;
+  mergeSort(l,md,arr);
+  mergeSort(md+1,r,arr);
+  merge(l,md,r,arr);
 }
-
-
-
-
-int main()
-{
-//    FIO
-//    freopen("input.txt","rt",stdin);
-//    freopen("output.txt","wt",stdout);
-    scanf("%d",&n);
-    for(int i=0; i<n; i++)
-        scanf("%d",arr+i);
-    mergeSort(0,n-1);
-    for(int i=0; i<n; i++)
-        printf("%d ",arr[i]);
-
+ 
+int main(){
+	int n;
+	cin>>n;
+	vector<int>arr(n);
+	for(int &x : arr) cin>>x;
+	mergeSort(0,n-1,arr);
+	for(int x : arr) cout<<x<<' ';
+	cout<<'\n';
+  return 0;
 }
