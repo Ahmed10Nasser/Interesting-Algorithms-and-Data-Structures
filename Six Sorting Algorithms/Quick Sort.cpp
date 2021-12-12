ef3cde4950067cc290a089f926a55ae971c83849
Ahmed Nasser Zaki <anm1611999@gmail.com>
2021-12-12 23:41:48 +0200

    Update Quick Sort.cpp

--- Six Sorting Algorithms/Quick Sort.cpp
+++ Six Sorting Algorithms/Quick Sort.cpp
@@ -10,98 +10,64 @@
 */
 
 #include<bits/stdc++.h>
-#define FIO ios_base::sync_with_stdio(0);cin.tie(0);
 using namespace std;
-typedef long long ll;
 
-int const N=2e5+5, M=1e3+5, MOD=1e9+7, OO=0x3f3f3f3f;
-
-int n;
-int arr[N];
-
-
-
-int random(int mn, int mx)
-{
+/*
+int random(int mn, int mx){
    static bool first=true;
-   if(first)
-   {
+   if(first){
       srand(time(NULL));
       first=false;
    }
    return mn+rand()%((mx+1)-mn);
 }
-
-
-pair<int,int> Partition(int l, int r)
-{
-    int m1=l-1,m2=l;
-    for(int i=l+1; i<=r; i++)
-    {
-        if(arr[i]<arr[m2])
-        {
-            swap(arr[++m1],arr[i]);
-            swap(arr[++m2],arr[i]);
-        }
-        else if(arr[i]==arr[m2])
-            swap(arr[++m2],arr[i]);
+*/
+ 
+pair<int,int>partition(int l, int r, vector<int>& arr){
+  int m1=l-1, m2=l;
+  for(int i=l+1; i<=r; i++){
+    if(arr[i]<arr[m2]){
+      swap(arr[++m1],arr[i]);
+      swap(arr[++m2],arr[i]);
     }
-    return {m1+1,m2};
+    else if(arr[i]==arr[m2])
+      swap(arr[++m2],arr[i]);
+  }
+  return {m1+1,m2};
 }
-
-
-// MAX stack space is log(n)
-void quickSort(int l, int r)
-{
-    while(l<r)
-    {
-
-//        swap(arr[l],arr[random(l,r)]); /// Randomized Pivot
-//        pair<int,int>md[]={{arr[l],l},{arr[(l+r)/2],(l+r)/2},{arr[r],r}}; /// Median Pivot
-//        sort(md,md+3);
-//        swap(arr[l],arr[md[1].second]);
-        pair<int,int> m1m2=Partition(l,r);
-        if(m1m2.first-l<r-m1m2.second)
-        {
-            quickSort(l,m1m2.first-1);
-            l=m1m2.second+1;
-        }
-        else
-        {
-            quickSort(m1m2.second+1,r);
-            r=m1m2.first-1;
-        }
-    }
+ 
+void quickSort(int l, int r, vector<int>& arr){
+  if(l>=r) return;
+ 
+  /*
+  // Randomized Pivot
+  int randomI=random(l,r);
+  swap(arr[randomI],arr[l]);
+  */
+ 
+  /*
+  // Median Pivot
+  int md=(l+r)/2;
+  if((arr[r]>=arr[md] && arr[r]<=arr[l]) || (arr[r]>=arr[l] && arr[r]<=arr[md]))
+    swap(arr[r],arr[l]);
+  else if((arr[md]>=arr[l] && arr[md]<=arr[r]) || (arr[md]>=arr[r] && arr[md]<=arr[l]))
+    swap(arr[md],arr[l]);
+  */
+ 
+  // First Pivot
+  pair<int,int>m1m2=partition(l,r,arr);
+  quickSort(l,m1m2.first-1,arr);
+  quickSort(m1m2.second+1,r,arr);
 }
-
-// MAX stack space is n and AVARAGE stack space is log(n)
-//void quickSort(int l, int r)
-//{
-//    if(l>=r)
-//        return;
-////    swap(arr[l],arr[random(l,r)]); /// Randomized Pivot
-////    pair<int,int>md[]={{arr[l],l},{arr[(l+r)/2],(l+r)/2},{arr[r],r}}; /// Median Pivot
-////    sort(md,md+3);
-////    swap(arr[l],arr[md[1].second]);
-//    pair<int,int> m1m2=Partition(l,r);
-//    quickSort(l,m1m2.first-1);
-//    quickSort(m1m2.second+1,r);
-//}
-
-
-
-
-int main()
-{
-//    FIO
-//    freopen("input.txt","rt",stdin);
-//    freopen("output.txt","wt",stdout);
-    scanf("%d",&n);
-    for(int i=0; i<n; i++)
-        scanf("%d",arr+i);
-    quickSort(0,n-1);
-    for(int i=0; i<n; i++)
-        printf("%d ",arr[i]);
-    puts("");
-    return 0;
+ 
+ 
+int main(){
+  int n;
+  cin>>n;
+  vector<int>arr(n);
+  for(int &x : arr) cin>>x;
+  quickSort(0,n-1,arr);
+  for(int x : arr) cout<<x<<' ';
+  cout<<'\n';
+  return 0;
 }
