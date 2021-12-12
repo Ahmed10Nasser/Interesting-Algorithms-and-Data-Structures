17a2a2971fac723d4cdce066274228940eaaa211
Ahmed Nasser Zaki <anm1611999@gmail.com>
2021-12-13 00:56:45 +0200

    Update Heap Sort.cpp

--- Sorting Algorithms/Heap Sort.cpp
+++ Sorting Algorithms/Heap Sort.cpp
@@ -10,65 +10,40 @@
 */
 
 #include<bits/stdc++.h>
-#define FIO ios_base::sync_with_stdio(0);cin.tie(0);
 using namespace std;
-typedef long long ll;
 
-int const N=1e2+5, M=17+5, MOD=1e9+7, OO=0x3f3f3f3f;
-
-int n,sz;
-int arr[N];
-
-
-void siftDown(int i)    /// O(log(n))
-{
+void siftDown(int i, int heapSz, vector<int>& arr){
     int mxi=i;
-    int l=2*i;
-    if(l<=sz && arr[l]>arr[mxi])
-        mxi=l;
-    int r=2*i+1;
-    if(r<=sz && arr[r]>arr[mxi])
-        mxi=r;
-    if(mxi!=i)
-    {
+    int l=2*(i+1)-1;
+    if(l<heapSz && arr[l]>arr[mxi]) mxi=l;
+    int r=2*(i+1);
+    if(r<heapSz && arr[r]>arr[mxi]) mxi=r;
+    if(mxi!=i){
         swap(arr[i],arr[mxi]);
-        siftDown(mxi);
+        siftDown(mxi,heapSz,arr);
     }
 }
 
+void heapSort(vector<int>& arr){
+  // Buliding Heap
+  int heapSz=arr.size();
+  for(int i=arr.size()/2-1; i>=0; i--)
+    siftDown(i,heapSz,arr);
 
-void buildHeap()    /// O(n)
-{
-    for(int i=n/2; i>0; i--)
-        siftDown(i);
+  // Sort by poping the min each time
+  for(int i=0; i<arr.size()-1; i++){
+    swap(arr[--heapSz],arr[0]);
+    siftDown(0,heapSz,arr);
+  }
 }
 
-void heapSort()     /// O(nlog(n))
-{
-    buildHeap();
-    for(int i=1; i<n; i++)
-    {
-        swap(arr[sz--],arr[1]);
-        siftDown(1);
-    }
-}
-
-
-
-int main()
-{
-//    FIO
-//    freopen("input.txt","rt",stdin);
-//    freopen("output.txt","wt",stdout);
-    scanf("%d",&n);
-    sz=n;
-    for(int i=1; i<=n; i++)
-        scanf("%d",arr+i);
-    heapSort();
-    for(int i=1; i<=n; i++)
-        printf("%d ",arr[i]);
-    puts("");
-
-
-    return 0;
+int main(){
+  int n;
+  cin>>n;
+  vector<int>arr(n);
+  for(int &x : arr) cin>>x;
+  heapSort(arr);
+  for(int x : arr) cout<<x<<' ';
+  cout<<'\n';
+  return 0;
 }
