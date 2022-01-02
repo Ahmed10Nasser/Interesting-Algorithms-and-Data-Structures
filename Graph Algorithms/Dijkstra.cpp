8f6bbcc1fa356ded5a3076a2a5eaa89beea95d49
Ahmed Nasser Zaki <anm1611999@gmail.com>
2022-01-02 22:31:24 +0200

    Update Dijkstra.cpp

--- Graph Algorithms/Dijkstra.cpp
+++ Graph Algorithms/Dijkstra.cpp
@@ -1,5 +1,4 @@
-/// Ahmed Nasser Mohamed
-/// 10/08/2020
+// Ahmed Nasser Zaki
 
 /*
 --Problem description : find the shortest paths form a node to all other nodes
@@ -9,62 +8,69 @@
 */
 
 #include<bits/stdc++.h>
-#define FIO ios_base::sync_with_stdio(0);cin.tie(0);
 using namespace std;
-typedef long long ll;
 
-int const N=1e5+5, M=1e5, MOD=1e9+7, OO=0x3f3f3f3f;
+const int MAX=1e8+1;
 
-int n,m,u,v,w;
-vector<int>dis;
-vector<pair<int,int>>adj[N];
+struct Node;
+struct Edge;
+
+struct Node{
+  int distance=MAX; 
+  vector<Edge>neighbours={}; 
+};
+
+struct Edge{
+  int weight=0;
+  Node* toNode=NULL;
+  Edge(int _weight, Node* _toNode) : weight(_weight), toNode(_toNode) {}
+  bool operator >(const Edge & e2) const{
+    return weight > e2.weight;
+  }
+};
 
 
-void dijkstra(int src)
-{
-    int u,v,c;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
-    dis[src]=0;
-    q.push({0,src});
-    while(!q.empty())
-    {
-        u=q.top().second;
-        c=q.top().first;
-        q.pop();
-        if(c!=dis[u])
-            continue;
-        for(pair<int,int> p : adj[u])
-        {
-            v=p.first;
-            c=p.second;
-            if(dis[v]>dis[u]+c)
-            {
-                dis[v]=dis[u]+c;
-                q.push({dis[v],v});
-            }
-        }
+void Dijkstra(Node* src){
+  priority_queue<Edge, vector<Edge>, greater<Edge>>q;
+  src->distance=0;
+  q.push(Edge(src->distance,src));
+  while(!q.empty()){
+    Edge tp=q.top();
+    q.pop();
+
+    int soFarWeight=tp.weight; 
+    Node* node=tp.toNode; 
+    if(soFarWeight != node->distance) continue;
+
+    for(auto& edge : node->neighbours){
+      int weight=edge.weight;
+      Node* neighbour=edge.toNode;
+      if(neighbour->distance > soFarWeight+weight){
+        neighbour->distance=soFarWeight+weight;
+        q.push(Edge(neighbour->distance, neighbour));
+      }
     }
+  }
 }
 
 
-
-int main()
-{
-//    FIO
-//    freopen("input.txt","rt",stdin);
-//    freopen("output.txt","wt",stdout);
-    scanf("%d %d",&n, &m);
-    for(int i=0; i<m; i++)
-    {
-        scanf("%d %d %d",&u, &v, &w);
-        adj[u].push_back({v,w});
-        adj[v].push_back({u,w});
-    }
-    dis=vector<int>(n+1,OO);
-    dijkstra(1);
-    for(int i=1; i<=n; i++)
-        printf("%d %d\n",i,dis[i]);
-
-
-    return 0;
+int main(){
+  int n,m,src;
+  cin>>n>>m>>src;
+  src--;
+  vector<Node*> nodes(n);
+  for(auto& node : nodes) node=new Node;
+  while(m--){
+    int u,v,c;
+    cin>>u>>v>>c;
+    u--,v--;
+    nodes[u]->neighbours.push_back(Edge(c,nodes[v]));
+    nodes[v]->neighbours.push_back(Edge(c,nodes[u]));
+  }
+  Dijkstra(nodes[src]);
+  for(int i=0; i<n; i++)
+    cout<<nodes[i]->distance<<'\n';
+  
+  return 0;
+  
 }
